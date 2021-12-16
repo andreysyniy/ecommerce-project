5615a8f33414409546b098902dd3f18086fd6872
Andres S <andreysyniy@gmail.com>
2021-12-16 21:03:49 +0200

    Stylization registration form

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -136,6 +138,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- shop/forms.py
+++ shop/forms.py
@@ -11,5 +11,5 @@ class SingUpForm(UserCreationForm):
 
   class Meta:
     model = User
-    fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
+    fields = ('first_name', 'last_name', 'username', 'password1', 'password2', 'email')
     
